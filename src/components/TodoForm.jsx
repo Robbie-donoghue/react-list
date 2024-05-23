2cc00ce9f678e59e914624868a3627907db5b329
Robbie <Leon.Wh1te@proton.me>
2024-05-23 14:44:17 +0100

    with setValue and addTodo passed as parent prop

--- src/components/TodoForm.jsx
+++ src/components/TodoForm.jsx
@@ -1,10 +1,26 @@
-export const TodoForm = () => {
+import { useState } from "react";
+//import addTodo prop
+export const TodoForm = ({ addTodo }) => {
+  //keep track of what user is typing
+  //empty string on initial render
+  const [value, setValue] = useState("");
+  //need to add finc that captures value when we submit form
+  const handleSubmit = (e) => {
+    //prevent default
+    e.preventDefault();
+    addTodo(value);
+    setValue(""); //after form submits set value to empty string
+  };
   return (
-    <form className="TodoForm">
+    //need to add finc that captures value when we submit form
+    <form className="TodoForm" onSubmit={handleSubmit}>
       <input
         type="text"
         className="todo-input"
+        //get user input with handleChange
+        value={value} // value = value in state
         placeholder="name of task"
+        onChange={(e) => setValue(e.target.value)}
       ></input>
       <button type="submit" className="todo-btn">
         Add Task
--- src/components/TodoWrapper.jsx
+++ src/components/TodoWrapper.jsx
@@ -1,9 +1,21 @@
 import { TodoForm } from "./TodoForm";
-
+import { useState } from "react";
+import { v4 as uuidv4 } from "uuid";
+uuidv4();
 export const TodoWrapper = () => {
+  //need to pass value from TodoForm to other components, create the state in the parent component, this one! -> use props to pass the state from TodoForm to TodoWrapper
+  const [todos, setTodos] = useState([]);
+  const addTodo = (todo) => {
+    //spread opp to make copy
+    setTodos([
+      ...todos,
+      { id: uuidv4(), task: todo, completed: false, isEditing: false },
+    ]);
+    console.log(todos);
+  };
   return (
     <div className="TodoWrapper">
-      <TodoForm />
+      <TodoForm addTodo={addTodo} />
     </div>
   );
 };
